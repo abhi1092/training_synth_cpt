d280df34d9ed6c9955e08e532b831ad76d72c1bd
Abhishek Bhandwaldar <abhi1092@gmail.com>
2024-12-16 15:53:07 -0500

    change tokenizer to meta llama 8b
    Signed-off-by: Abhishek Bhandwaldar <abhi1092@gmail.com>

--- src/instructlab/training/tokenize_qa_sft.py
+++ src/instructlab/training/tokenize_qa_sft.py
@@ -100,7 +100,7 @@ def tokenize_and_save(tokenizer: AutoTokenizer):
 
 if __name__ == '__main__':
     # loading tokenizer
-    tokenizer = AutoTokenizer.from_pretrained('ibm-granite/granite-3.0-8b-base', use_fast=True)
-    tokenizer.model_max_length=4000 # this is to hide the token_len>2048 wraning
+    tokenizer = AutoTokenizer.from_pretrained('meta-llama/Meta-Llama-3-8B', use_fast=True)
+    tokenizer.model_max_length=2**20 # this is to hide the token_len>2048 wraning
     # tokenizing the dataset
     tokenize_and_save(tokenizer)
