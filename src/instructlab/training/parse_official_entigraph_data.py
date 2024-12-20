47e53b495397a776c5f6295e816e2dd9b2291a18
Wenlong Zhao <wenlong.zhao.97@gmail.com>
2024-12-20 16:32:08 -0500

    parse EntiGraph 455M data to reproduce their numbers

---
+++ src/instructlab/training/parse_official_entigraph_data.py
@@ -0,0 +1,20 @@
+from datasets import load_dataset
+from io_utils import *
+import os
+
+ds = load_dataset("zitongyang/entigraph-quality-corpus")
+
+os.makedirs("data/quality_entigraph_gpt-4-turbo", exist_ok=True)
+for example in ds['train']:
+    output = []
+    output_path = f'data/quality_entigraph_gpt-4-turbo/{example["uid"]}.json'
+    
+    entities = example['entity'].split("<|entityseptoekn|>")
+    entities = [entity.strip() for entity in entities]
+    output.append(entities)
+    
+    texts = example['entigraph'].split("<|entigraphseptoekn|>")
+    texts = [text.strip() for text in texts]
+    output.extend(texts)
+    
+    jdump(output, output_path)
